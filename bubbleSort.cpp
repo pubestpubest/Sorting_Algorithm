a3123538f050cc0b1568efa955ee686f176a1a2b
pubespubest <pubespubest@gmail.com>
2022-12-13 14:49:05 +0700

    unfinished bubble

---
+++ bubbleSort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int *bubbleSort(int len,int num[]){
+    int i=0,j,temp;
+    while (i<len){
+        j=0;
+        while(j<len-1){
+            if (num[j]>num[j+1]){
+                temp=num[j];
+                num[j]=num[j+1];
+                num[j+1]=num[j];
+            }
+            j++;
+        }
+        i++;
+    }
+    return num;
+}
+
+int main(int argc, char const *argv[]){
+    int len;
+    cout<<"Enter length of input :";
+    cin>>len;
+    int num[len],i=0;
+    //Input loop
+    while (i<len){
+        cout<<"Enter input :";
+        cin>>num[i++];
+    }i=0;
+
+    int *address_num=bubbleSort(len,num);
+
+    //Output loop
+    cout<<"Output :";
+    while (i<len){
+        cout<<address_num[i]<<" ";
+        i++;
+    }
+    return 0;
+}
